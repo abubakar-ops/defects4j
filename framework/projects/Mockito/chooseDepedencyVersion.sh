c8df4b78c6abbede287e733ca9abe76bd112f10b
jose <zecapistolas@gmail.com>
2019-06-17 17:37:31 -0700

    Updated path to Mockito project directory.

--- framework/projects/Mockito/chooseDepedencyVersion.sh
+++ framework/projects/Mockito/chooseDepedencyVersion.sh
@@ -2,12 +2,13 @@
 # Gregory Gay (greg@greggay.com)
 # Ensures the correct version of the byte-buddy dependency is used at compile time
 # $1 = workDir
-# $2 = D4J_HOME
+
+HERE=$(cd `dirname $0` && pwd)
 
 cd $1
 ./gradlew dependencies >> tmpDepend.txt
 if grep -q buddy tmpDepend.txt; then
     version=`cat tmpDepend.txt | grep buddy | cut -d: -f 3 | head -1`
-    cp $2/framework/projects/Mockito/byte-buddy/byte-buddy-$version.jar $1/compileLib/
+    cp $HERE/byte-buddy/byte-buddy-$version.jar $1/compileLib/
 fi
 rm tmpDepend.txt
