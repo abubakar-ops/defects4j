f31f1cd303c9b5610ad28c4166495c12a7cf254f
jose <zecapistolas@gmail.com>
2019-07-09 11:15:25 -0700

    The files with the list of failing tests are now named with bug id.

--- framework/bug-mining/promote-to-db.pl
+++ framework/bug-mining/promote-to-db.pl
@@ -120,11 +120,12 @@ system("mkdir -p $OUTPUT_DIR/$PID");
 my $project = Project::create_project($PID);
 my $dbh_trigger = DB::get_db_handle($TAB_TRIGGER, $WORK_DIR);
 
-my @rev_specific_files = ("failing_tests/<rev>", "build_files/<rev>");
+my @rev_specific_files = ("build_files/<rev>");
 my @id_specific_files = ("loaded_classes/<id>.src", "loaded_classes/<id>.test",
                             "modified_classes/<id>.src", "modified_classes/<id>.test",
                             "patches/<id>.src.patch", "patches/<id>.test.patch",
-                            "trigger_tests/<id>", "relevant_tests/<id>");
+                            "trigger_tests/<id>", "relevant_tests/<id>",
+                            "failing_tests/<id>");
 my @generic_files_and_directories_to_replace = ("build.xml.patch", "${PID}.build.xml", "lib");
 my @generic_files_to_append = ("dir-layout.csv");
 
